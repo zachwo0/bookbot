1d3e079f2ab00f78b179a717d7df095ba9f2ecca
zachwo0 <zwodom0@gmail.com>
2025-05-20 20:07:13 -0400

    Edit main.py and stats.py, remove get file function from stats.py and added it to main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from stats import get_word_count
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
 
 def main():
     book_text = get_book_text('./books/frankenstein.txt')
--- stats.py
+++ stats.py
@@ -1,9 +1,3 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    return file_contents
-
-
 def get_word_count(booktext):
     words_list = booktext.split()
     return len(words_list)
